ea229147438466cc7a8d61bfc7291447589db108
Drew Sartorius <duckydesign@gmail.com>
2016-03-28 10:39:18 -0400

    Hits sized by mass

--- JS_D3.js
+++ JS_D3.js
@@ -38,13 +38,26 @@ var graticule = d3.geo.graticule();
       .style("fill",function(d,i){return color(d.color = d3.max(neighbors[i], function(n){return countries[n].color;}) + 1 | 0);});
     
     $.get(dataUrl,function(results){
-      var meteorHits = JSON.parse(results);
+      var meteorHits = JSON.parse(results).features;
+      var massVal = function(d){return +d.properties.mass;};
+      var validVals = meteorHits.filter(function(a){
+        if(a.geometry !== null){
+          return true;
+        }
+        return false;
+      });
+      var scale = d3.scale.linear().range([1,20])
+        .domain([d3.min(validVals,massVal),
+                 d3.max(validVals,massVal)]);
+      function getRadius(d){
+        return scale(d.properties.mass);
+      };
       
       svg.selectAll(".hits")
-        .data(meteorHits.features).enter()
+        .data(validVals).enter()
         .append("circle")
-        .attr("r",3)
-        .style("fill","black")
+        .attr("r",getRadius)
+        .style("fill","rgba(0,0,0,0.5)")
         .attr("transform",function(d){return "translate("+projection(d.geometry.coordinates)+")"});
     });
   });
